e7f0b2805ddd34e44e7470dad98e3870f725a9d8
jsaleix <julian.saleix@gmail.com>
2021-07-14 16:20:50 +0200

    CustomProvider works

--- client/sdk/providers/custom/CustomProvider.php
+++ client/sdk/providers/custom/CustomProvider.php
@@ -14,21 +14,63 @@ class CustomProvider extends ProviderAbstract implements ProviderInterface
     }
 
     public function handleCodeType():void{
-        //handle
+        ["code" => $code, "state" => $state] = $_GET;
+        $result = file_get_contents("http://sdk-oauth-server:8081/token?"
+            . "grant_type=authorization_code"
+            . "&client_id=" . $this->client_id
+            . "&client_secret=" . $this->client_secret
+            . "&code=" . $code);
+        $token = json_decode($result, true);
+
+        if($token['access_token'])
+        {
+            var_dump($this->getInfos($token['access_token']));
+        }else{
+            echo 'Une erreur est survenue';
+            return;
+        }
     }
 
     public function handlePasswordType():void{
-        //handle
+        if( isset($_POST['username']) && isset($_POST['password']))
+        {
+            [ 'username' => $username, 'password' => $pwd ] = $_POST;
+
+            $result = file_get_contents("http://sdk-oauth-server:8081/token?"
+                . "grant_type=password"
+                . "&client_id=" . $this->client_id
+                . "&client_secret=" . $this->client_secret
+                . "&username=" . $username 
+                . "&password=" . $pwd
+            );
+            $content = json_decode($result, true);
+            
+            if(isset($content["access_token"]))
+            {
+                echo var_dump($this->getInfos($content["access_token"]));
+                exit;
+            }else{
+                echo 'Une erreur est survenue';
+            }
+        }
     }
 
     public function getInfos($token):array{
-        //infos
+        $context = stream_context_create([
+            'http' => [
+                'method' => "GET",
+                'header' => "Authorization: Bearer " . $token
+            ]
+        ]);
+        $result = file_get_contents("http://sdk-oauth-server:8081/api", false, $context);
+        $user = json_decode($result, true);
+        return $user;
     }
 
     public function getLinks(): string{
         $html = '<h2>Custom Oauth-server</h2>';
         $html .= '<p>Login with password</p><br>';
-        $html .= "<form method='POST' action='/password'>
+        $html .= "<form method='POST' action='/custom/password'>
                     <input type='text' name='username' placeholder='username'/>
                     <input type='password' name='password' placeholder='password'/>
                     <input type='submit' value='Login'/>
@@ -42,11 +84,22 @@ class CustomProvider extends ProviderAbstract implements ProviderInterface
         return $html;
     }
 
-    public function getErrorMessage(): string{
-        
+    public function getErrorMessage(): void{
+        echo 'Une erreur est survenue';
+        return;
     }
 
     public function handleRoute($route = null): ?array{
+        switch($route){
+            case 'password':
+                return $this->handlePasswordType();
+            case 'success':
+                return $this->handleCodeType();
+            case 'error':
+                return $this->getErrorMessage();
+            case 'token':
+                return $this->getInfos();
+        }
         return null;
     }
 
